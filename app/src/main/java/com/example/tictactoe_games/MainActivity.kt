bec6a5a4fc7a9d406681f7660832c940cd2001b1
mukramm_ <99308248+mmukramm@users.noreply.github.com>
2024-01-04 01:52:23 +0800

    feat:
    make starter logic to play the game

--- app/src/main/java/com/example/tictactoe_games/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe_games/MainActivity.kt
@@ -5,29 +5,30 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import kotlin.properties.Delegates
 
 class MainActivity : AppCompatActivity() {
 
-    var isPlay = true
-    var isYou = true
+    private var isPlay = false
+    private var isYou by Delegates.notNull<Boolean>()
 
-    lateinit var playAgainBtn : Button
-    lateinit var gameResultTV : TextView
+    private lateinit var playBtn : Button
+    private lateinit var gameResultTV : TextView
 
-    lateinit var item00 : ImageView
-    lateinit var item01 : ImageView
-    lateinit var item02 : ImageView
-    lateinit var item10 : ImageView
-    lateinit var item11 : ImageView
-    lateinit var item12 : ImageView
-    lateinit var item20 : ImageView
-    lateinit var item21 : ImageView
-    lateinit var item22 : ImageView
+    private lateinit var item00 : ImageView
+    private lateinit var item01 : ImageView
+    private lateinit var item02 : ImageView
+    private lateinit var item10 : ImageView
+    private lateinit var item11 : ImageView
+    private lateinit var item12 : ImageView
+    private lateinit var item20 : ImageView
+    private lateinit var item21 : ImageView
+    private lateinit var item22 : ImageView
 
     private var board = Array(3){IntArray(3)}
 
     private fun initComponent(){
-        playAgainBtn = findViewById(R.id.playAgainBtn)
+        playBtn = findViewById(R.id.playBtn)
         gameResultTV = findViewById(R.id.gameResultTV)
 
         item00 = findViewById(R.id.item00)
@@ -55,9 +56,64 @@ class MainActivity : AppCompatActivity() {
         initComponent()
         initBoard()
 
-        if (isPlay){
-            playAgainBtn.visibility = Button.INVISIBLE
-            gameResultTV.visibility = TextView.INVISIBLE
+        gameResultTV.visibility = TextView.INVISIBLE
+
+        playBtn.setOnClickListener {
+            isPlay = true
+            if (isPlay){
+                playGame()
+            }
+        }
+    }
+
+    private fun playGame() {
+        isYou = true
+        gameResultTV.visibility = TextView.INVISIBLE
+        playBtn.visibility = Button.INVISIBLE
+        initOnclick()
+    }
+
+    private fun insertItem(item : ImageView, i: Int, j: Int) {
+
+
+        if (isYou){
+            board[i][j] = 1
+            item.setImageResource(R.drawable.x_icon)
+        }else{
+            board[i][j] = 2
+            item.setImageResource(R.drawable.o_icon)
+        }
+        item.isEnabled = false
+        isYou = !isYou
+    }
+
+    private fun initOnclick() {
+        item00.setOnClickListener{
+            insertItem(it as ImageView, 0,0)
+        }
+        item01.setOnClickListener{
+            insertItem(it as ImageView, 0,1)
+        }
+        item02.setOnClickListener{
+            insertItem(it as ImageView, 0,2)
+        }
+        item10.setOnClickListener{
+            insertItem(it as ImageView, 1,0)
+        }
+        item11.setOnClickListener{
+            insertItem(it as ImageView, 1,1)
+        }
+        item12.setOnClickListener{
+            insertItem(it as ImageView, 1,2)
+        }
+        item20.setOnClickListener{
+            insertItem(it as ImageView, 2,0)
+        }
+        item21.setOnClickListener{
+            insertItem(it as ImageView, 2,1)
+        }
+        item22.setOnClickListener{
+            insertItem(it as ImageView, 2,2)
         }
     }
 
